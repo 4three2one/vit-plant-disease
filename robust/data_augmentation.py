4bfa1b908358f5c22f92ff2ed891b00ab763a310
xjw <1375464896@qq.com>
2022-08-07 12:37:10 +0800

    同步代码

--- robust/data_augmentation.py
+++ robust/data_augmentation.py
@@ -33,6 +33,31 @@ def addGaussianNoise(image, percetage):
         G_Noiseimg[temp_x][temp_y][np.random.randint(3)] = np.random.randn(1)[0]
     return G_Noiseimg
 
+def gaussian_noise(img, mean, sigma):
+    '''
+    此函数用将产生的高斯噪声加到图片上
+    传入:
+        img   :  原图
+        mean  :  均值
+        sigma :  标准差
+    返回:
+        gaussian_out : 噪声处理后的图片
+        noise        : 对应的噪声
+    '''
+    # 将图片灰度标准化
+    img = img / 255
+    # 产生高斯 noise
+    noise = np.random.normal(mean, sigma, img.shape)
+    # 将噪声和图片叠加
+    gaussian_out = img + noise
+    # 将超过 1 的置 1，低于 0 的置 0
+    gaussian_out = np.clip(gaussian_out, 0, 1)
+    # 将图片灰度范围的恢复为 0-255
+    gaussian_out = np.uint8(gaussian_out*255)
+    # 将噪声范围搞为 0-255
+    # noise = np.uint8(noise*255)
+    return gaussian_out, noise # 这里也会返回噪声，注意返回值
+
 
 # 昏暗
 def darker(image, percetage=0.9):
@@ -77,11 +102,98 @@ def rotate(image, angle, center=None, scale=1.0):
 def flip(image):
     flipped_image = np.fliplr(image)
     return flipped_image
+# 随机裁剪
+def cutout(img, length=50,overimg=''):
+    h=img.shape[0]
+    w = img.shape[1]
+    mask = np.ones((h, w))
+    b, g, r = cv2.split(img)
+    y = np.random.randint(h)
+    x = np.random.randint(w)
+    y1 = np.clip(y - length // 2, 0, h)
+    y2 = np.clip(y + length // 2, 0, h)
+    x1 = np.clip(x - length // 2, 0, w)
+    x2 = np.clip(x + length // 2, 0, w)
+    mask[y1: y2, x1: x2] = 0
+    rm = r * mask
+    gm = g * mask
+    bm = b * mask
+    merged = cv2.merge([bm, gm, rm])
+    ###img for overlap
+    if overimg != '':
+        over = cv2.imread(overimg)
+        overresize = cv2.resize(over, (w, h), interpolation=cv2.INTER_CUBIC)
+        mask2 = np.zeros((h, w))
+        bo, go, ro = cv2.split(overresize)
+        mask2[y1: y2, x1: x2] = 1
+        b = bo * mask2
+        g = go * mask2
+        r = ro * mask2
+        merged2 = cv2.merge([b, g, r])
+        return merged + merged2
+    return merged
+def saltpepper():
+    for rate in np.linspace(0.01, 0.1, 10):
+        for dir in tqdm(os.listdir(file_dir)):
+            category = os.path.basename(dir)
+            for img_name in os.listdir(file_dir + dir):
+                file_out = os.path.join(f'{file_dir[0:-1]}-SaltAndPepper',"{:.3f}".format(rate), category) + '/'
+                if not os.path.exists(file_out):
+                    os.makedirs(file_out)
+                img_path = os.path.join(file_dir, category, img_name)
+                img = cv2.imread(img_path)
+                # cv2.imwrite(file_out + img_name,img)
+                # 镜像
+                # flipped_img = flip(img)
+                # cv2.imwrite(file_dir + img_name[0:-4] + '_fli.jpg', flipped_img)
 
-
+                # 增加噪声
+                img_salt = SaltAndPepper(img, rate)
+                cv2.imwrite(file_out + img_name[0:-4] + '_p.jpg', img_salt)
+def blur():
+    for rate in np.linspace(1,10,10):
+        for dir in tqdm(os.listdir(file_dir)):
+            category = os.path.basename(dir)
+            for img_name in os.listdir(file_dir + dir):
+                file_out = os.path.join(f'{file_dir[0:-1]}-blur', f'{rate}', category) + '/'
+                if not os.path.exists(file_out):
+                    os.makedirs(file_out)
+                img_path = os.path.join(file_dir, category, img_name)
+                img = cv2.imread(img_path)
+                # 增加噪声
+                blur = cv2.blur(img, (int(rate), int(rate)))
+                # #      cv2.GaussianBlur(图像，卷积核，标准差）
+                cv2.imwrite(file_out + img_name[0:-4] + '_blur.jpg', blur)
+def GaussianBlur():
+    for rate in np.arange(1, 15, 2):
+        for dir in tqdm(os.listdir(file_dir)):
+            category = os.path.basename(dir)
+            for img_name in os.listdir(file_dir + dir):
+                file_out = os.path.join(f'{file_dir[0:-1]}-GaussianBlur', f'{rate}', category) + '/'
+                if not os.path.exists(file_out):
+                    os.makedirs(file_out)
+                img_path = os.path.join(file_dir, category, img_name)
+                img = cv2.imread(img_path)
+                # 增加噪声
+                blur = cv2.GaussianBlur(img, (int(rate), int(rate)),0)
+                # #      cv2.GaussianBlur(图像，卷积核，标准差）
+                cv2.imwrite(file_out + img_name[0:-4] + '_gb.jpg', blur)
+def occlusion():
+    for rate in np.arange(60, 100, 5):
+        for dir in tqdm(os.listdir(file_dir)):
+            category = os.path.basename(dir)
+            for img_name in os.listdir(file_dir + dir):
+                file_out = os.path.join(f'{file_dir[0:-1]}-occlusion61', f'{rate}', category) + '/'
+                if not os.path.exists(file_out):
+                    os.makedirs(file_out)
+                img_path = os.path.join(file_dir, category, img_name)
+                img = cv2.imread(img_path)
+                cut = cutout(img, rate)
+                # #      cv2.GaussianBlur(图像，卷积核，标准差）
+                cv2.imwrite(file_out + img_name[0:-4] + '_gblur.jpg', cut)
 # 图片文件夹路径
 file_dir = './test-tomato-img/'
-
+blur()
 # for img_name in os.listdir(file_dir):
 #     img_path = file_dir + img_name
 #     img = cv2.imread(img_path)
@@ -92,23 +204,6 @@ file_dir = './test-tomato-img/'
 #     cv2.imwrite(file_dir + img_name[0:-4] + '_r90.jpg', rotated_90)
 #     rotated_180 = rotate(img, 180)
 #     cv2.imwrite(file_dir + img_name[0:-4] + '_r180.jpg', rotated_180)
-for rate in np.linspace(0,0.1,11):
-    for dir in tqdm(os.listdir(file_dir)):
-        category=os.path.basename(dir)
-        for img_name in os.listdir(file_dir+dir):
-            file_out=os.path.join(f'{file_dir[0:-1]}-output-SaltAndPepper',f'{rate}',category)+'/'
-            if not os.path.exists(file_out):
-                os.makedirs(file_out)
-            img_path=os.path.join(file_dir,category,img_name)
-            img = cv2.imread(img_path)
-            #cv2.imwrite(file_out + img_name,img)
-            # 镜像
-            #flipped_img = flip(img)
-            #cv2.imwrite(file_dir + img_name[0:-4] + '_fli.jpg', flipped_img)
-
-            # 增加噪声
-            img_salt = SaltAndPepper(img, rate)
-            cv2.imwrite(file_out + img_name[0:-4] + '_salt.jpg', img_salt)
             # img_gauss = addGaussianNoise(img, 0.3)
             # cv2.imwrite(file_out + img_name[0:-4] + '_noise.jpg', img_gauss)
 
--- test-model-robust.py
+++ test-model-robust.py
@@ -13,11 +13,18 @@ from utils.my_dataset import MyDataSet
 # from vit_model import vit_base_patch16_224 as create_model
 from utils.utils import read_data, evaluate
 #from vit_model.vit_model import  vit_base_patch16_224 as model
-from cnn_model.resnet import  resnet50 as model
+from vit_model.epe_msa_vit import  vit_base_patch16_224 as model
+#from cnn_model.resnet import  resnet50 as model
+#from cnn_model.densenet import  densenet121 as model
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+model = model(num_classes=11)
+model.to(device)
+model_weight_path = "./robust/model-weight/epe-msa-bias8-vit-aug-tomato-8-2-from-scratch_weights.pth"
+model.load_state_dict(torch.load(model_weight_path, map_location=device))
+root="./robust/test-tomato-img-SaltAndPepper"
 
-root="./robust/test-tomato-img-output-SaltAndPepper"
+
 dirs=[os.path.join(root, cla) for cla in os.listdir(root)]
 acc=[]
 for dir in dirs:
@@ -42,19 +49,21 @@ for dir in dirs:
                                                collate_fn=test_dataset.collate_fn)
 
 
-    model = model(num_classes=38)
-    model.to(device)
-    model_weight_path = "./robust/model-weight/resnet.pth"
-    model.load_state_dict(torch.load(model_weight_path, map_location=device))
+
     model.eval()
 
 
     val_acc = evaluate(model=model,
                                  data_loader=test_loader,
                                  device=device,
-                                 epoch=1)
+                                 epoch=dir)
 
     acc.append(val_acc[1])
-pass
-
-
+modelname=str(model).split('(')[0]
+res=f"./robust/noise/{modelname}-{root.split('/')[2].split('-')[-1]}.txt"
+if os.path.exists(res):
+    os.remove(res)
+f = open(res, 'a')
+for i in acc:
+    f.write(format(i, '.4f')+' ')
+f.close()
