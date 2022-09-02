3bb8bd4f66be245e0aafec34c7ff8b3c126e0c72
leavepast <1375464896@qq.com>
2022-09-02 16:25:56 +0800

    画vit从零学习的性能图

--- draw-img/showdata-fromscrach.py
+++ draw-img/showdata-fromscrach.py
@@ -16,7 +16,7 @@ def main():
     vit_acc=np.array(vit)[:, 2]
     epoch=np.array(data_bias1_msa)[:, 1]+1
     # coding=utf-8
-    plt.rcParams['font.sans-serif'] = ['Arial']  # 如果要显示中文字体,则在此处设为：SimHei
+    plt.rcParams['font.sans-serif'] = ['SimHei']  # 如果要显示中文字体,则在此处设为：SimHei
     plt.rcParams['axes.unicode_minus'] = False  # 显示负号
 
     # label在图示(legend)中显示。若为数学公式,则最好在字符串前后添加"$"符号
@@ -34,12 +34,12 @@ def main():
     plt.plot(epoch, vit_acc, marker='^',linestyle = 'dotted', color="darkorange", label="ViT", linewidth=1.2)
 
     # group_labels = ['Top 0-5%', 'Top 5-10%', 'Top 10-20%', 'Top 20-50%', 'Top 50-70%', ' Top 70-100%']  # x轴刻度的标识
-    plt.xticks(epoch,  fontsize=12, fontweight='bold')  # 默认字体大小为10
+    plt.xticks(epoch[::4],  fontsize=12, fontweight='bold')  # 默认字体大小为10
     plt.yticks(fontsize=12, fontweight='bold')
     # plt.title("example", fontsize=12, fontweight='bold')  # 默认字体大小为12
     plt.xlabel("迭代次数", fontsize=13, fontweight='bold')
     plt.ylabel("训练准确率", fontsize=13, fontweight='bold')
-    plt.xlim(3, 51)  # 设置x轴的范围
+    plt.xlim(0, 51)  # 设置x轴的范围
     plt.ylim(0.8, 1)
 
     # plt.legend()          #显示各曲线的图例
